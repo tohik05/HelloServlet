a7ca17aa65e03e2bb042691f9aae2bd2d0cf27b3
Anton Khoroshok <tohik05@gmail.com>
2023-09-18 12:26:28 +0300

    Edited HelloServlet

--- src/main/java/com/example/helloservlet/HelloServlet.java
+++ src/main/java/com/example/helloservlet/HelloServlet.java
@@ -10,12 +10,13 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello Java!";
+        message = "First commit, hello java!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
